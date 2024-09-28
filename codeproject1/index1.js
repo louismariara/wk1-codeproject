82b73f4ecc44fd5f319276513eee1abf06965207
Louis Githogori Mariara <louismariara@gmail.com>
2024-09-28 11:16:22 +0300

    Add challenge1

--- codeproject1/index1.js
+++ codeproject1/index1.js
@@ -1,16 +1,16 @@
 function studentgrade(marks) {
-if (marks > 79) {
-    return "A";
+if (marks > 79) {    
+    return "A";      //    for marks greater than 79
 }
-else if (marks >= 60) {
-    return "B";
+else if (marks >= 60 &&  marks <= 79) {
+    return "B";     // for marks between 60 to 79
 }
-else if (marks >=49){
-    return "C";
+else if (marks >=50 && marks <= 59){
+    return "C";     //    for marks between 49 to 59
 }
-else if (marks >=40){
-    return "D";
+else if (marks >=40 && marks <= 49){
+    return "D";    //  for marks between 40 to 49
 }
 else
-  return 'E';
+    return 'E';   //    for marks less than 40
 }
